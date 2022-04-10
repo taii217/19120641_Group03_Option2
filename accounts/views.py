3dc9916ccbccda19fe5d1700c7a018e7b77ec769
taii217 <taiinguyen217@gmail.com>
2022-04-10 14:35:26 +0700

    video 2.1

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    return HttpResponse('Home')
+
+def products(request):
+    return HttpResponse('products')
+
+def customer(request):
+    return HttpResponse('customer')
--- crm1/urls.py
+++ crm1/urls.py
@@ -14,18 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Home page")
-
-def contact(request):
-    return HttpResponse("Contact page")
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home),
-    path('about/', contact),
+    path('', include('accounts.urls')),
 ]
