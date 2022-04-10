6cb0d3f303a9306933a5988c9e21018d7253336e
taii217 <taiinguyen217@gmail.com>
2022-04-10 14:24:03 +0700

    video 2.0

--- crm1/urls.py
+++ crm1/urls.py
@@ -16,6 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Home page")
+
+def contact(request):
+    return HttpResponse("Contact page")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home),
+    path('about/', contact),
 ]
