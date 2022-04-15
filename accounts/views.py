f2d8d660c36220636854a709ff39cbc353ca7946
taii217 <taiinguyen217@gmail.com>
2022-04-15 21:01:25 +0700

    video 10.0

--- accounts/views.py
+++ accounts/views.py
@@ -4,15 +4,31 @@ from django.http import HttpResponse
 from .models import *
 # Create your views here.
 def home(request):
-    return render(request,'accounts\dashboard.html')
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+    orders_count = orders.count()
+    customers_count = customers.count()
+    orders_delivered_count = orders.filter(status ="Delivered").count()
+    orders_pending_count = orders.filter(status ="Pending").count()
+
+    context ={'customers':customers,
+                'orders':orders,
+                'orders_count':orders_count,
+                'customers_count':customers_count,
+                'orders_delivered_count':orders_delivered_count,
+                'orders_pending_count':orders_pending_count
+            }
+    
+    
+    return render(request,'accounts\dashboard.html',context)
 
 def products(request):
-    return render(request,'accounts\products.html')
+    products = Product.objects.all()
+    context ={'products':products}
+    return render(request,'accounts\products.html',context)
 
 def customer(request,pk_test):
     customer = Customer.objects.get(id = pk_test)
-    if(not customer):
-        return render(request,'accounts\customer.html',context)
     orders = customer.order_set.all()
     order_count = orders.count()
     context = {'customer':customer, 'orders':orders, 'order_count':order_count}
