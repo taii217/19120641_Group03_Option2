a8a620e6d02f2497b953c558add7af8def2fafa2
taii217 <taiinguyen217@gmail.com>
2022-04-24 00:48:36 +0700

    report

--- crm1/settings.py
+++ crm1/settings.py
@@ -133,5 +133,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bbtram28@gmail.com'
-EMAIL_HOST_PASSWORD = 'Tram1234'
+EMAIL_HOST_USER = 'tai@gmail.com' # testing by own email is working 
+EMAIL_HOST_PASSWORD = 'Tai12345'
